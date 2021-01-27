c35c78f5c7df92e1c9d7140575d53cdf1e31fddc
madekot <mgolubcov@gmail.com>
2021-01-27 21:33:36 +0300

    использует векторный спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,7 @@ gulp.task("sprite", function () {
   return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
     .pipe(svgstore({inlineSvg: true}))
     .pipe(rename("sprite_auto.svg"))
-    .pipe(gulp.dest("build/img"));
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("html", function () {
